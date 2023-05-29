ffce1d88b0b1e3dbf897c7b4856825aa8d467174
giancwby <gcaedo.gc@gmail.com>
2023-05-29 16:27:20 +0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import sanity from "astro-sanity";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sanity({
+    projectId: 'evknzm0w',
+    dataset: 'production',
+    apiVersion: '2023-02-08',
+    useCdn: false,
+  })]
+});
